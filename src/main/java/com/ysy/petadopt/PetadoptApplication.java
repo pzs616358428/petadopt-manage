7a829e15ee0d3f6baffeb4fa036781ed4e90e937
庞振硕 <616358428@qq.com>
2019-04-09 22:05:20 +0800

    Initial commit

---
+++ src/main/java/com/ysy/petadopt/PetadoptApplication.java
@@ -0,0 +1,13 @@
+package com.ysy.petadopt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetadoptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetadoptApplication.class, args);
+    }
+
+}
