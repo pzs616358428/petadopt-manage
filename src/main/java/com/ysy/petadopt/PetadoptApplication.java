9a5f0e56ea14e61c0a9296805ceb2ce10270914d
庞振硕 <616358428@qq.com>
2019-04-14 23:36:08 +0800

    完成左侧菜单栏

--- src/main/java/com/ysy/petadopt/PetadoptApplication.java
+++ src/main/java/com/ysy/petadopt/PetadoptApplication.java
@@ -3,6 +3,9 @@ package com.ysy.petadopt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 项目启动类
+ */
 @SpringBootApplication
 public class PetadoptApplication {
 
